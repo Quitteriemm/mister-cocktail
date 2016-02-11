76bd38375e61359e18c10026ea5f5120bba8b31e
Quitterie Mathelin Moreaux <q.mathelin@gmail.com>
2016-02-11 17:14:22 +0100

    mister-cocktial with rails finish

---
+++ app/models/ingredient.rb
@@ -0,0 +1,6 @@
+class Ingredient < ActiveRecord::Base
+  has_many :doses
+  has_many :cocktails, through: :doses, dependent: :restrict_with_exception
+  validates :name, presence: true, uniqueness: true
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+sugar = Ingredient.create(name: "sugar")
+salt = Ingredient.create(name: "salt")
+chocolate = Ingredient.create(name: "chocolate")
+pepper = Ingredient.create(name: "pepper")
+
+mojito = Cocktail.create(name: "Mojito")
+capirinha = Cocktail.create(name: "Capirinha")
+olivas = Cocktail.create(name: "Olivas")
+
+d3 = Dose.new(description: "3cl")
+d5 = Dose.new(description: "5cl")
+d3bis = Dose.new(description: "3")
+
+d3.ingredient = pepper
+d3.cocktail = mojito
+d3.save
